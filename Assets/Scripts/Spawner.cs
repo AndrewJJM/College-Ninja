1e5e3b294ad3eda71ead66b5310f393c268fe566
Alessiaa01 <occhionero.alessia01@gmail.com>
2023-09-15 17:07:58 +0200

    prova

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    private Collider spawnArea;
+    
+    public GameObject[] fruitPrefabs;
+    
+    public float minSoawnDelay= 0.25f;
+    public float maxSpawnDelay= 1f;
+    
+    public float minAngle = -15f;
+    public float maxAngle= 15f;
+    
+    public float minForce=18f;
+    public float maxforce=22f;
+    
+    public float maxLifetime= 5f;
+    
+    private void Awake()
+    {
+      spawnArea = GetComponent <Collider>();
+    }
+
+ private void OnEnable()
+{
+  StartCoroutine(Spawn());
+}
+
+private void OnDisable()
+{
+    StopAllCoroutines();
+}
+
+private IEnumerator Spawn()
+{
+    
+}
+
+}
