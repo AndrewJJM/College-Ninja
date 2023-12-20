f5019854818c62a228a210483cd0dd4709506dc6
Alessiaa01 <occhionero.alessia01@gmail.com>
2023-12-20 16:33:00 +0100

    VolumeSettings

--- Assets/VolumeSettings.cs
+++ Assets/VolumeSettings.cs
@@ -6,4 +6,10 @@ public class VolumeSettings : MonoBehaviour
 {
     [SerializeField] private AudioMixer myMixer;
     [SerializeField] private Slider musicSlider;
+
+    public void SetMusicVolume()
+    {
+        float volume = musicSlider.value;
+        myMixer.SetFloat("music", volume);
+    }
 }
