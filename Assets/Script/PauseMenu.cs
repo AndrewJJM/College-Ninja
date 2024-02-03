9a971cabd2a7aae9768a8066f59743ead02e3261
AndrewJJ <andjohnmurugan100@gmail.com>
2024-02-03 09:04:26 +0100

    Sensore frutta non tagliata + conteggio 5 vite
    il codice dell'ui è un po' a caso, gamemanager non dovrebbe gestirsi l'ui... però funziona :)

--- Assets/Script/PauseMenu.cs
+++ Assets/Script/PauseMenu.cs
@@ -5,10 +5,14 @@ public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
     [SerializeField] GameObject gameOverMenu;
+    [SerializeField] GameObject scoreUI;
+    [SerializeField] GameObject pauseButton;
 
     public void Pause( )
     {
         pauseMenu.SetActive(true);
+        scoreUI.SetActive(false);
+        pauseButton.SetActive(false);
         Time.timeScale = 0;
     }
 
@@ -28,6 +32,8 @@ public class PauseMenu : MonoBehaviour
     public void Resume()
     {
         pauseMenu.SetActive(false);
+        scoreUI.SetActive(true);
+        pauseButton.SetActive(true);
         Time.timeScale = 1;
     }
 
